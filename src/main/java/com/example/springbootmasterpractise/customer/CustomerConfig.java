3e780486da6ecfe260d3607596e9cb441789ad78
mahadi-bo4 <mahadihasanbo4@gmail.com>
2024-02-12 17:02:12 +0600

    Spring Profiles & yaml

--- src/main/java/com/example/springbootmasterpractise/customer/CustomerConfig.java
+++ src/main/java/com/example/springbootmasterpractise/customer/CustomerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfig {
@@ -11,10 +12,21 @@ public class CustomerConfig {
     @Value("${app.useFakeRepository:false}")
     private Boolean useFakeRepository;
 
+    @Value("${info.app.description}")
+    private String companyDescription;
+
+    private Environment environment;
+
+    public CustomerConfig(Environment env) {
+        environment = env;
+    }
+
     @Bean
     CommandLineRunner commandLineRunner(){
         return args -> {
             System.out.println("Command line runner running....");
+            System.out.println(companyDescription);
+            System.out.println(environment.getProperty("info.app.name"));
         };
     }
 
